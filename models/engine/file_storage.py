1e72df6b9a582d4965fe5b3948fb348e8ce4a1ef
M3siCS <hadeelkaddy86@gmail.com>
2024-05-21 15:53:04 -0700

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,28 +7,32 @@ from os.path import exists
 
 
 class FileStorage:
-    __file_path = 'file.json'
-    __objects = {}
+    """Class for FileStorage"""
+
+    __file_path = "file.json"
+    __objects = dict()
 
     def all(self):
+        """return the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
-        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+        """sets obj in __objects with key <obj class name>.id"""
+        self.__objects[obj.__class__.__name__ + '.' + obj.id] = obj
 
     def save(self):
-        with open(self.__file_path, 'w') as f:
-            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, f)
+        """serialize __objects to JSON file"""
+        temp = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, mode='w') as jsonfile:
+            json.dump(temp, jsonfile)
 
     def reload(self):
-        try:
-            with open(self.__file_path, 'r') as f:
-                data = json.load(f)
-                for key, value in data.items():
-                    cls_name = value['__class__']
-                    self.__objects[key] = eval(cls_name)(**value)
-        except FileNotFoundError:
-            pass
-
-    def classes(self):
-        return {"BaseModel": BaseModel, "User": User}
+        """deserializes the JSON file to __objects"""
+        if exists(self.__file_path):
+            with open(self.__file_path) as jsonfile:
+                decereal = json.load(jsonfile)
+            for key, value in decereal.items():
+                cls_name = value['__class__']
+                cls = globals().get(cls_name)
+                if cls:
+                    self.__objects[key] = cls(**value)
